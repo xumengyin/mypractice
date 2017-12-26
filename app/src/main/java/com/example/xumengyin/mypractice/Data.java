b9e56abaae3ad857b7c0cb7ca2d2fa612adab885
xumengyin <1150280107@qq.com>
2017-12-26 22:34:12 +0800

    second

---
+++ app/src/main/java/com/example/xumengyin/mypractice/Data.java
@@ -0,0 +1,15 @@
+package com.example.xumengyin.mypractice;
+
+/**
+ * Created by xumengyin on 2017/12/25.
+ */
+
+public class Data {
+    public String title;
+    public Class activity;
+
+    public Data(String title, Class activity) {
+        this.title = title;
+        this.activity = activity;
+    }
+}
