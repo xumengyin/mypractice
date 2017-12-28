70c493312b84ad93927c66f1db203f24db001b19
xumengyin <xumengyin>
2017-12-28 18:41:17 +0800

    tt

--- app/src/main/java/com/example/xumengyin/mypractice/MainActivity.java
+++ app/src/main/java/com/example/xumengyin/mypractice/MainActivity.java
@@ -5,27 +5,43 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 
+import com.example.xumengyin.mypractice.testactivity.BaseActivity;
+import com.example.xumengyin.mypractice.testactivity.ContraintLayoutActivity;
+import com.example.xumengyin.mypractice.testactivity.WebViewTestActivity;
+
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+
+public class MainActivity extends BaseActivity
+{
 
     static List<Data> listdata=new ArrayList<>();
     static {
-//        listdata.add()
+        listdata.add(new Data("webview js test ", WebViewTestActivity.class));
+        listdata.add(new Data("refresh test ", ContraintLayoutActivity.class));
     }
+    @BindView(R.id.recycleview)
     RecyclerView recyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(new Myadapter());
     }
+
+    @Override
+    protected int getcontentView()
+    {
+        return R.layout.activity_main;
+    }
+
     private class MyHolder extends RecyclerView.ViewHolder
     {
         Button button;
@@ -36,18 +52,23 @@ public class MainActivity extends AppCompatActivity {
     }
     private class Myadapter extends RecyclerView.Adapter<MyHolder>
     {
+        LayoutInflater inflater;
+        public Myadapter()
+        {
+            inflater=LayoutInflater.from(getBaseContext());
+        }
 
         @Override
         public MyHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
-            MyHolder holder =new MyHolder(View.inflate(getApplicationContext(),R.layout.menu_item,null));
+            MyHolder holder =new MyHolder(inflater.inflate(R.layout.menu_item,parent,false));
             return holder;
         }
 
         @Override
         public void onBindViewHolder(MyHolder holder, final int position) {
             holder.button.setText(listdata.get(position).title);
-            holder.itemView.setOnClickListener(new View.OnClickListener() {
+            holder.button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
                     getBaseContext().startActivity(new Intent(getBaseContext(),listdata.get(position).activity));
